72d8e573e62fe5ee2fdb3363da04e12b9931bca6
unknown <ts1sio@SIO301-16.sio.bts>
2023-03-16 17:18:24 +0100

    Creation du modele Caserne

---
+++ src/main/java/modeles/Caserne.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modeles;
+
+/**
+ *
+ * @author ts1sio
+ */
+public class Caserne {
+    
+    
+}
