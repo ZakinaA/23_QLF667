24eeff0a95ff513d2492996b1829c88375ae7ff6
HRVNoa <hervieu.noa@gmail.com>
2023-03-16 17:52:34 +0100

    Ajout GETTERS&SETTERS Pompier

--- src/main/java/modeles/Pompier.java
+++ src/main/java/modeles/Pompier.java
@@ -15,9 +15,59 @@ public class Pompier {
     private String nom;
     private String prenom;
     private String dateNaissance;
-    private String numeroBip;
+    private int numeroBip;
     
     
+    public Pompier(){}
+    
+    public Pompier(int id, String nom, String prenom, String dateNaissance, int numeroBip){
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateNaissance = dateNaissance;
+        this.numeroBip = numeroBip;
+    }
 
+    /*GETTERS & SETTERS*/
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(String dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public int getNumeroBip() {
+        return numeroBip;
+    }
+
+    public void setNumeroBip(int numeroBip) {
+        this.numeroBip = numeroBip;
+    }
     
 }
