023a4fed74aa82293d2427afd750aaca4722c962
HRVNoa <hervieu.noa@gmail.com>
2023-03-23 17:14:41 +0100

    ajout du modele grade

---
+++ src/main/java/modeles/Grade.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modeles;
+
+/**
+ *
+ * @author noa.hervieu
+ */
+public class Grade {
+    
+    private String code;
+    private String libelle;
+
+    
+    //Constructeur
+    public Grade() {
+    }
+
+    public Grade(String code, String libelle) {
+        this.code = code;
+        this.libelle = libelle;
+    }
+
+    //GETTER & SETTER
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    
+    
+    
+}
