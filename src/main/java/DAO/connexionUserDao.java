48fc79ab9af62777fc1187764a25401af6cd7e68
HRVNoa <hervieu.noa@gmail.com>
2023-04-04 11:52:33 +0200

    Ajout frm connexion et connexionUserDAO -> A faire "tester la connexion"

---
+++ src/main/java/DAO/connexionUserDao.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author noa.hervieu
+ */
+public class connexionUserDao {
+    
+    static PreparedStatement requete = null;
+    static private int nbCompteFound = 0;
+    
+    public static boolean connexionUser(Connection connection, String username, String password){
+        try
+        {
+            requete = connection.prepareStatement("SELECT * FROM POMPIER WHERE POM_USERNAME = ? AND POM_MDP = ?");
+            requete.setString(1, username);
+            requete.setString(2, password);
+            
+            ResultSet rs = requete.executeQuery();
+            if (rs.next()){
+                nbCompteFound = 1;
+            }
+            InitConnexion.fermerConnexion(rs);
+            InitConnexion.fermerConnexion(requete);
+            
+            if (nbCompteFound == 0) { 
+                return false;
+            } else{
+                return true;
+            }
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+}
