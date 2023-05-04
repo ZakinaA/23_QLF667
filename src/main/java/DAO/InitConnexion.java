99f40d20763512083718cbaf03c55915c8bf5702
LouaneR <roussel.louane@gmail.com>
2023-05-04 15:35:56 +0200

    inscription dans la base de données

---
+++ src/main/java/DAO/InitConnexion.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author noa.hervieu
+ */
+public class InitConnexion {
+    public static Connection connection = null;
+    public static Statement st = null;
+    public static ResultSet rs = null;
+    
+    // Méthode de création et d'ouverture de la connexion
+    public static Connection ouvrirConnexion(){
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");                  
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();         
+        }     
+        try {
+            //obtention de la connexion
+            connection= DriverManager.getConnection("jdbc:mariadb://172.20.177.77:3306/23QLF667","ADM_QLF667","mpQlf667");
+            System.out.println("Connexion OK");
+           
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }            
+        return connection ;
+     
+    }
+    // Méthode de fermeture du resultset
+    public static void fermerConnexion(ResultSet rs)
+    {
+        if(rs!=null)
+        {
+            try
+            {
+                rs.close();
+            }
+            catch(Exception e)
+            {
+        	System.out.println("Erreur lors de la fermeture d’une connexion dans fermerConnexion(ResultSet)");
+            }
+        }
+    }
+
+    // Méthode de fermeture du statement
+    public static void fermerConnexion(Statement stmt)
+    {
+        if(stmt!=null)
+        {
+            try
+            {
+                stmt.close();
+            }
+            catch(Exception e)
+            {
+                System.out.println("Erreur lors de la fermeture d’une connexion dans fermerConnexion(Statement)");
+            }
+        }
+    }
+
+    /// Méthode de fermeture de la connexion
+    public static void fermerConnexion(Connection con)
+    {
+        if(con!=null)
+        {
+            try
+            {
+                con.close();
+                System.out.println("Fermeture Connexion OK");
+            }
+            catch(Exception e)
+            {
+                System.out.println("Erreur lors de la fermeture d’une connexion dans fermerConnexion(Connection)");
+            }
+        }
+    } 
+    
+}
